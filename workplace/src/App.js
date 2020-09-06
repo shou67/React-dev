5bcf94df5b16db2ac9eca2d488adfc8a186def48
shou67 <shouk1704@gmail.com>
2020-09-06 20:15:56 +0900

    compareing jsx and js with code

--- workplace/src/App.js
+++ workplace/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+//↓JSX
 class App extends Component {
   render(){
-    return <div><h1> Hello, World</h1></div>;
+    return <h1>Hello, World!</h1>;
   }
 }
+
+
+//↓JS
+// class App extends Component {
+//   render(){
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, World!!"
+//     );
+//   }
+// }
+
 
 export default App;
